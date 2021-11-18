0c68bff4f4e4da53d7aad148884a0f797db4846c
dwwr <derek.warnerreyes@olioapps.com>
2021-11-18 13:49:58 -0800

    calls mint on commit, commit msg in process.argv

--- src/mint.ts
+++ src/mint.ts
@@ -1,34 +1,34 @@
 const Rareterm = require('rareterm.node')
 
 const mint = async () => {
-
+  console.log(process.argv)
   // 1. Initialize a rarepress object from Rareterm
-  const rarepress = new Rareterm()
+  // const rarepress = new Rareterm()
   // 2. Connect to a rinkeby rarenet node
-  await rarepress.init({ host: "https://eth.rarenet.app/v1" })
+  // await rarepress.init({ host: "https://eth.rarenet.app/v1" })
 
   // 3. Import a web image to rarepress file system
   // let cid = await rarepress.fs.add("")
 
   // 4. Create and save a token on rarepress
-  let token = await rarepress.token.create({
-    type: "ERC721",
-    metadata: {
-      name: "Test Minting to existing Wallet",
-      description: "Imma just island boi",
-      // image: "/ipfs/" + cid
-    }
-  })
+  // let token = await rarepress.token.create({
+  //   type: "ERC721",
+  //   metadata: {
+  //     name: "Test Minting to existing Wallet",
+  //     description: "Imma just island boi",
+  //     // image: "/ipfs/" + cid
+  //   }
+  // })
 
   // 5. Publish the image on public IPFS
   // await rarepress.fs.push(cid)
   // 6. Publish the metadata on public IPFS
-  await rarepress.fs.push(token.tokenURI)
-
+  // await rarepress.fs.push(token.tokenURI)
+  // console.log("token", token)
   // 7. Publish the token to the marketplace
-  let receipt = await rarepress.token.send(token)
+  // let receipt = await rarepress.token.send(token)
 
-  console.log("# SENT", receipt)
-  console.log(`Check your token at: https://rarible.com/token/${receipt.id}`)
+  // console.log("# SENT", receipt)
+  // console.log(`Check your token at: https://rarible.com/token/${receipt.id}`)
 }
 mint()
